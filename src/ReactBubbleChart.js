3d55ae6b9ce1d207be0dbeec8547e0b26b92e0c2
Andre Mas <andrejohn.mas@gmail.com>
2017-07-18 16:51:58 -0400

    Fixes issue #10, whereby an update happens before a mount

--- src/ReactBubbleChart.js
+++ src/ReactBubbleChart.js
@@ -152,6 +152,10 @@ class ReactBubbleChart extends React.Component {
 
   /** When we update, update our friend, the bubble chart */
   componentDidUpdate() {
+    // Apparently updates can happen before a mount, so deal with this scenario
+    if (!this.bubbleChart) {
+       this.bubbleChart = new ReactBubbleChartD3(this.getDOMNode(), this.getChartState());
+    }
     this.bubbleChart.update(this.getDOMNode(), this.getChartState());
   }
 
